7efe98e7128f3d915aa19893d82a70eeb09bb1ce
Ritesh99 <ritesh.success@gmail.com>
2016-08-26 17:22:06 +0530

    First Git Test Demo

---
+++ gittest/src/test/java/guru99/gittest/AppTest.java
@@ -0,0 +1,26 @@
+package guru99.gittest;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class AppTest {		
+    private WebDriver driver;		
+	@Test				
+	public void testEasy() {	
+		driver.get("https://www.google.co.in/");  
+		String title = driver.getTitle();				 
+		AssertJUnit.assertTrue(title.contains("Google")); 		
+	}	
+	@BeforeTest
+	public void beforeTest() {	
+	    driver = new FirefoxDriver();  
+	}		
+	@AfterTest
+	public void afterTest() {
+		driver.quit();			
+	}		
+}	
